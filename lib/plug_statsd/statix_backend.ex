6c5b6f0f6219d8d4e4b0b742df027234515792c9
shayon <shyjavadi@gmail.com>
2019-02-12 13:40:46 -0800

    Add debug logging to statix backend

--- lib/plug_statsd/statix_backend.ex
+++ lib/plug_statsd/statix_backend.ex
@@ -1,24 +1,32 @@
 if Code.ensure_loaded?(Statix) do
   defmodule Plug.Statsd.StatixBackend do
     def increment(name, 1, tags) do
+      IO.inspect(tags, label: "Debug tags")
       increment(name, 1.0, tags: tags)
     end
 
     def increment(name, rate, tags) when is_float(rate) do
+      IO.inspect(tags, label: "Debug tags")
       get_conn().increment(name, 1, sample_rate: rate, tags: tags)
     end
 
     def timing(name, elapsed, 1, tags) do
+      IO.inspect(tags, label: "Debug tags")
       timing(name, elapsed, 1.0, tags: tags)
     end
 
     def timing(name, elapsed, rate, tags) when is_float(rate) do
+      IO.inspect(tags, label: "Debug tags")
       get_conn().timing(name, elapsed, sample_rate: rate, tags: tags)
     end
 
-    def histogram(name, elapsed, 1, tags), do: histogram(name, elapsed, 1.0, tags: tags)
+    def histogram(name, elapsed, 1, tags) do
+      IO.inspect(tags, label: "Debug tags")
+      histogram(name, elapsed, 1.0, tags: tags)
+    end
 
     def histogram(name, elapsed, rate, tags) when is_float(rate) do
+      IO.inspect(tags, label: "Debug tags")
       get_conn().histogram(name, elapsed, sample_rate: rate, tags: tags)
     end
 
